58dee6b697af53536d4698fd0cdd7b8c31a6ed9a
denesrafael <denes.rafael@gmail.com>
2024-03-03 19:16:48 -0300

    Painel Administrativo

--- erp/admin.py
+++ erp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from erp.models import Funcionario, Produto, Venda
+
+
+class FuncionarioAdmin(admin.ModelAdmin): ...
+
+
+class ProdutoAdmin(admin.ModelAdmin): ...
+
+
+class VendaAdmin(admin.ModelAdmin): ...
+
+
+admin.site.register(Funcionario, FuncionarioAdmin)
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Venda, VendaAdmin)
